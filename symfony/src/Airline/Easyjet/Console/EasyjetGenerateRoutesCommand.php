962d4d6ca227b02ecced8370261666c1e26f2a3e
Mikulas Vesely <mikface@gmail.com>
2022-02-02 11:08:46 +0100

    easyjet done

--- symfony/src/Airline/Easyjet/Console/EasyjetGenerateRoutesCommand.php
+++ symfony/src/Airline/Easyjet/Console/EasyjetGenerateRoutesCommand.php
@@ -6,11 +6,13 @@ namespace App\Airline\Easyjet\Console;
 
 use App\Airline\Repository\Domain\AirlineRepository;
 use App\Airport\Domain\AirportRepository;
+use App\Core\Service\Curl;
 use App\Route\Repository\Domain\RouteRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use DateTimeImmutable;
 
 #[AsCommand(
     name: 'easyjet:generate-routes',
@@ -18,17 +20,34 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class EasyjetGenerateRoutesCommand extends Command
 {
     private const DATA_URL = 'https://www.easyjet.com/EN/linkedAirportsJSON';
+    private const EASYJET_ICAO = 'EZY';
 
     public function __construct(
         private AirlineRepository $airlineRepository,
         private AirportRepository $airportRepository,
-        private RouteRepository $routeRepository
-    ) {
+        private RouteRepository   $routeRepository
+    )
+    {
         parent::__construct();
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) : int
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $airline = $this->airlineRepository->findByIcao(self::EASYJET_ICAO);
+        $airports = Curl::performSingleGet(self::DATA_URL);
+        preg_match('/^var ac_la = (.*);/', $airports, $matches);
+        $airportJsonString = str_replace("'", '"', $matches[1]);
+        foreach (json_decode($airportJsonString, true) as $connection) {
+            $parts = explode("|", $connection);
+            $routeEnd = new DateTimeImmutable($parts[3]);
+            $now = new DateTimeImmutable();
+            if (str_starts_with($parts[0], "*") || str_starts_with($parts[1], "*") || $routeEnd < $now) {
+                continue;
+            }
+            $airportA = $this->airportRepository->findByIata($parts[0]);
+            $airportB = $this->airportRepository->findByIata($parts[1]);
+            $this->routeRepository->addIfNotExists($airline, $airportA, $airportB);
+        }
         return Command::SUCCESS;
     }
 }
