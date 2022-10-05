87f57f6c624dfd575a6c2adc1cf7a1d3e505d537
Mikulas Vesely <mikface@gmail.com>
2022-10-05 03:03:09 +0200

    ryanair import prices command redone (monthly api endpoint)

--- symfony/src/Airline/Ryanair/Console/GetPricesCommand.php
+++ symfony/src/Airline/Ryanair/Console/GetPricesCommand.php
@@ -21,7 +21,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-use function array_key_exists;
 use function count;
 use function intval;
 use function sprintf;
@@ -35,12 +34,9 @@ final class GetPricesCommand extends Command
     private const ARGUMENT_OFFSET = 'offset';
     private const GENERATION_WEEKS_FORWARD = 13;
     private const GENERATION_INTERVAL = 'P' . self::GENERATION_WEEKS_FORWARD . 'W';
-    private const INTERVAL = 'P7D';
-    private const START_INTERVAL = 'P3D';
-    private const URL_BASE = 'https://www.ryanair.com/api/booking/v4/cs-cz/availability?' .
-    'ADT=2&CHD=0&DateIn=%s&DateOut=%s&Destination=%s&Disc=0&INF=0&Origin=%s&TEEN=0' .
-    '&promoCode=&IncludeConnectingFlights=false' .
-    '&FlexDaysBeforeIn=3&FlexDaysIn=3&RoundTrip=true&FlexDaysBeforeOut=3&FlexDaysOut=3&ToUs=AGREED';
+    private const INTERVAL = 'P1M';
+    private const URL_BASE = 'https://www.ryanair.com/api/farfnd/3/roundTripFares' .
+    '/%s/%s/cheapestPerDay?market=en-gb&outboundMonthOfDate=%s&inboundMonthOfDate=%s';
 
     public function __construct(
         private AirlineRepository $airlineRepository,
@@ -62,7 +58,7 @@ final class GetPricesCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $io->note('Starting Ryanair price import...');
         $interval = new DateInterval(self::INTERVAL);
-        $now = $this->clock->now()->add(new DateInterval(self::START_INTERVAL));
+        $now = $this->clock->now();
         $stopDate = $this->clock->now()->add(new DateInterval(self::GENERATION_INTERVAL));
         $airline = $this->airlineRepository->findByIcao(Airline::RYANAIR->getInfo()->icao);
         $limit = $input->getArgument(self::ARGUMENT_LIMIT);
@@ -80,12 +76,12 @@ final class GetPricesCommand extends Command
                 $stringDate = $now->format('Y-m-d');
                 $url = sprintf(
                     self::URL_BASE,
-                    $stringDate,
-                    $stringDate,
-                    $route->getAirportB()->getIata(),
                     $route->getAirportA()->getIata(),
+                    $route->getAirportB()->getIata(),
+                    $stringDate,
+                    $stringDate,
                 );
-                $this->savePrices(Curl::performSingleGetAndDecode($url, useProxy: true), $route);
+                $this->savePrices(Curl::performSingleGetAndDecode($url), $route);
             }
 
             $now = $now->add($interval);
@@ -99,43 +95,36 @@ final class GetPricesCommand extends Command
     /** @param array<string, string> $prices */
     private function savePrices(array $prices, Route $route) : void
     {
-        if (! array_key_exists('trips', $prices)) {
-            return;
+        foreach ($prices['outbound']['fares'] as $normalTrip) {
+            $price = $normalTrip['price'];
+            if ($price === null) {
+                continue;
+            }
+
+            $priceDTOs[] = new Price(
+                $route,
+                $price['currencyCode'],
+                $price['value'],
+                RouteDirection::NORMAL,
+                $this->clock->now()->modify($normalTrip['departureDate']),
+                $this->clock->now()->modify($normalTrip['arrivalDate']),
+            );
         }
 
-        $currency = $prices['currency'];
-        foreach ($prices['trips'][0]['dates'] as $normalTrip) {
-            foreach ($normalTrip['flights'] as $flight) {
-                if (! array_key_exists('regularFare', $flight)) {
-                    continue;
-                }
-
-                $priceDTOs[] = new Price(
-                    $route,
-                    $currency,
-                    $flight['regularFare']['fares'][0]['amount'],
-                    RouteDirection::NORMAL,
-                    $this->clock->now()->modify($flight['time'][0]),
-                    $this->clock->now()->modify($flight['time'][1]),
-                );
+        foreach ($prices['inbound']['fares'] as $reverseTrip) {
+            $price = $reverseTrip['price'];
+            if ($price === null) {
+                continue;
             }
-        }
 
-        foreach ($prices['trips'][1]['dates'] as $reverseTrip) {
-            foreach ($reverseTrip['flights'] as $flight) {
-                if (! array_key_exists('regularFare', $flight)) {
-                    continue;
-                }
-
-                $priceDTOs[] = new Price(
-                    $route,
-                    $currency,
-                    $flight['regularFare']['fares'][0]['amount'],
-                    RouteDirection::REVERSE,
-                    $this->clock->now()->modify($flight['time'][0]),
-                    $this->clock->now()->modify($flight['time'][1]),
-                );
-            }
+            $priceDTOs[] = new Price(
+                $route,
+                $price['currencyCode'],
+                $price['value'],
+                RouteDirection::REVERSE,
+                $this->clock->now()->modify($reverseTrip['departureDate']),
+                $this->clock->now()->modify($reverseTrip['arrivalDate']),
+            );
         }
 
         $this->priceImporter->importFromPriceDTOs($priceDTOs ?? []);
